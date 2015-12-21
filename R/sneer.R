38c4857f0ab96d5ed701a938c8b25764e6e1fcd0
James Melville <jlmelville@gmail.com>
2015-12-20 19:37:20 -0800

    Document package.

--- R/sneer.R
+++ R/sneer.R
@@ -1,132 +1,145 @@
 #' sneer: Stochastic Neighbor Embedding Experiments in R.
 #'
-#' A package for similarity-preserving embedding.
+#' A package for exploring similarity-preserving embedding and related forms
+#' of dimensionality reduction. Its main goal is to implement multiple
+#' embedding methods within a single framework so comparison between them is
+#' easier, without worrying about the effect of differences in preprocessing,
+#' optimization and heuristics. Additionally it provides a way to explore the
+#' importance of those effects on the performance of individual methods. For
+#' example, how much of an effect does the early exaggeration trick in t-SNE
+#' have on the embedding?
 #'
-#' @section Examples
+#' First, some examples of how to use the package for embedding. A longer
+#' discussion of the package structure follows.
 #'
 #' @examples
+#' # Do t-SNE on the iris dataset with the same options as the t-SNE paper
+#' # and initialize from random. Use generic plot function, displaying the first
+#' # two characters of the "Species" factor for the points. Explicitly choose
+#' # t-SNE as the method.
+#' tsne_iris <- embed_sim(iris[, 1:4],
+#'                        method = tsne(),
+#'                        opt = tsne_opt(),
+#'                        init_inp = make_init_inp(perplexity = 25),
+#'                        init_out = make_init_out(stdev = 1e-4)
+#'                        tricks = tsne_tricks(),
+#'                        reporter = make_reporter(plot_fn = make_plot(
+#'                          iris, "Species", make_label(2))))
 #'
-#' tsne_iris <- embed_sim(iris[, 1:4], perplexity = 25, method = tsne(),
-#'                        preprocess = make_preprocess(
-#'                          range_scale_matrix = TRUE),
-#'                        init_out = make_init_out(from_PCA = TRUE),
-#'                        epoch = make_epoch(plot_func = make_iris_plot()))
+#' # Do ASNE on the synthetic the s1k dataset (10 overlapping 9D Gaussian blobs),
+#' # Autoscale the input, use PCA to initialize the embedding, and use
+#' # Nesterov Accelerated Gradient to do the optimization.
+#' asne_s1k <- embed_sim(s1k[, 1:9],
+#'                       method = asne(),
+#'                       preprocess = make_preprocess(auto_scale = TRUE),
+#'                       init_inp = make_init_inp(perplexity = 50),
+#'                       init_out = make_init_out(from_PCA = TRUE),
+#'                       opt = bold_nag_opt(),
+#'                       reporter = make_reporter(
+#'                        plot_fn = make_plot(s1k, "Label")))
 #'
-
-
-#' tsne_iris <- embed_sim(iris[, 1:4], perplexity = 50, method = tsne(), preprocess = make_preprocess(range_scale_matrix = TRUE), init_out = make_init_out(), opt = make_opt(step_size = jacobs(inc_fn = partial(`+`, 0.2), dec_mult = 0.8, min_step_size = 0.1), update = no_momentum(), normalize_grads = FALSE), epoch = make_epoch(plot_func = make_iris_plot()))
-
-
-#' tsne_iris <- embed_sim(iris[, 1:4], perplexity = 50, method = tsne(),
-#' preprocess = make_preprocess(range_scale_matrix = TRUE),
-#' init_out = make_init_out(),
-#' opt = make_opt(step_size = jacobs(inc_fn = partial(`+`, 0.2),
-#' dec_mult = 0.8, min_step_size = 0.1), update = step_momentum(),
-#' normalize_grads = FALSE),
-#' tricks = make_tricks(early_exaggeration = TRUE),
-#' epoch = make_epoch(plot_func = make_iris_plot()))
-
+#' @seealso
+#' In writing this package, the emphasis has been on making it possible to
+#' implement multiple different methods and apply different optimization and
+#' preprocessing techniques to them. This has two downsides: no effort has been
+#' made to make this package fast or memory efficient; and the entire workflow
+#' of an embedding algorithm is spread out over multiple functions. If you
+#' just want to understand how, for example, t-SNE is implemented, it's better
+#' to look at the code in 'tsne' package
+#' \link{https://github.com/jdonaldson/rtsne}.
 #'
-#' tsne_iris <- prob_embed_f(iris[, 1:4], plot_func = iris_plot(),
-#'                           perplexity = 50,
-#'                           gradient = tsne(cost_func = kullback_leibler_cost),
-#'                           input_weight = sqrt_exp_weight(),
-#'                           range_scale_matrix = TRUE,
-#'                           Opt = opt(step_size = bold_driver(),
-#'                            update = nesterov_non_convex_momentum(),
-#'                           gradient = nesterov))
-#' Should converge to 0.05539
+#' @section Embedding:
+#' Currently, only the similarity-based embedding techniques related to
+#' Stochastic Neighbor Embedding (SNE) are supported: the popular t-Distributed
+#' Stochastic Neighbor Embedding (t-SNE) technique, and two older (and
+#' generally less useful) methods asymmetric SNE (ASNE, what was originally just
+#' called SNE) and symmetric SNE (SSNE).
 #'
-#' Removed 0 columns, 4 remaining
-#' Range Scaling
-#' sigma: Min. : 0.1268 |1st Qu. : 0.1462 |Median : 0.1694 |Mean : 0.1717 |3rd Qu. : 0.1998 |Max. : 0.2187 |
-#'  beta: Min. : 10.46 |1st Qu. : 12.53 |Median : 17.41 |Mean : 18.3 |3rd Qu. : 23.39 |Max. : 31.12 |
-#'  perps: Min. : 50 |1st Qu. : 50 |Median : 50 |Mean : 50 |3rd Qu. : 50 |Max. : 50 |
-#'  x2p: Min. : 0 |1st Qu. : 3.138e-05 |Median : 0.0006403 |Mean : 0.006667 |3rd Qu. : 0.008674 |Max. : 0.1286 |
-#'  Pjoint: Min. : 2.22e-16 |1st Qu. : 3.994e-07 |Median : 5.542e-06 |Mean : 4.444e-05 |3rd Qu. : 5.913e-05 |Max. : 0.000825 |
-#'  Shannon Entropy 8.961 Effective perp: 498.3 Psum 1
+#' A characteristic quality of these methods is that they don't attempt to
+#' preserve the input distances but input probabilities, which are based on
+#' the distances by some non-linear weighting function.
 #'
+#' The entry point for these similarity
+#' preserving embeddings is the function \code{embed_sim}. Apart from the
+#' description of the various parts of the embedding below, you can get more info
+#' and examples by looking up the help by running
 #'
-#' DEFAULTS: gaussian weight P, range scale, PCA, normalized gradients,
-#'  steepest descent
-#' + bold driver, no early exaggeration, no momentum tsne_iris <-
-#' prob_embed_f(iris[,1:4], plot_func=iris_plot(), perplexity=50,
-#' gradient=tsne(),export=c('In'))
-#' Range Scaling
-#' sigma: Min. : 0.05397 |1st Qu. : 0.06713 |Median : 0.08705 |Mean : 0.09248 |3rd Qu. : 0.1168 |Max. : 0.1493 |
-#' beta: Min. : 22.45 |1st Qu. : 36.66 |Median : 65.98 |Mean : 75.7 |3rd Qu. : 111 |Max. : 171.7 |
-#' perps: Min. : 50 |1st Qu. : 50 |Median : 50 |Mean : 50 |3rd Qu. : 50 |Max. : 50 |
-#' x2p: Min. : 0 |1st Qu. : 5.2e-08 |Median : 0.0001954 |Mean : 0.006667 |3rd Qu. : 0.01084 |Max. : 0.07498 |
-#' Pjoint: Min. : 2.22e-16 |1st Qu. : 1.834e-08 |Median : 2.548e-06 |Mean : 4.444e-05 |3rd Qu. : 7.232e-05 |Max. : 0.0004652 |
-#' Shannon Entropy 8.967
-#' Effective perp: 3921
-#' Psum 1
-#' PCA: 2 components explained 97.77% variance
-#' Epoch: Iteration #0 cost = 0.8882
-#' Epoch: Iteration #100 cost = 0.0767
-#' Epoch: Iteration #200 cost = 0.07115
-#' Epoch: Iteration #300 cost = 0.06898
-#' Epoch: Iteration #400 cost = 0.06776
-#' Epoch: Iteration #500 cost = 0.06699
-#' Epoch: Iteration #600 cost = 0.06646
-#' Epoch: Iteration #700 cost = 0.06607
-#' Epoch: Iteration #800 cost = 0.06578
-#' Epoch: Iteration #900 cost = 0.06557
-#' Epoch: Iteration #1000 cost = 0.06541
-#' Nbr overlap: Min.: 0.64 |1st Qu. : 0.9 |Median : 0.94 |Mean : 0.9367 |3rd Qu. : 1 |Max. : 1 |
-#' Nbr enrichment: Min. : 1.907 |1st Qu. : 2.682 |Median : 2.801 |Mean : 2.791 |3rd Qu. : 2.98 |Max. : 2.98 |
-#' rho: Min. : 0.3671 |1st Qu. : 0.9577 |Median : 0.9681 |Mean : 0.9592 |3rd Qu. : 0.9913 |Max. : 0.9982 |
-#' wrho: Min. : 0.6963 |1st Qu. : 0.9292 |Median : 0.9509 |Mean : 0.9439 |3rd Qu. : 0.9743 |Max. : 0.9966 |
-
-#' # Momentum and early exaggeration
-#' tsne_iris <- prob_embed_f(iris[, 1:4], plot_func = iris_plot(),
-#'                           perplexity = 50, gradient = tsne(),
-#'                           Opt = opt(update = step_momentum()),
-#'                           early_exaggeration = TRUE, export = c('In'))
+#' \code{?embed_sim}
 #'
-#' # Nesterov Accelerated Gradient
-#' asne_iris <- prob_embed_f(iris[, 1:4], plot_func = iris_plot(),
-#'                           perplexity = 50, gradient = asne(),
-#'                           Opt = opt(step_size = bold_driver(),
-#'                                     update = nesterov_non_convex_momentum(),
-#'                                     gradient = nesterov),
-#'                           export = c('In'))
+#' and then further help by looking up the functions mentioned in the
+#' \code{See Also} section.
 #'
-#' # JSE s1k
-#' jse_s1k <- prob_embed_f(s1k[, 1:9], plot_func = make_plot_func(s1k, 'Label'),
-#'                         perplexity = 30,
-#'                         gradient = hajse(kappa = 0.5, alpha = 1.5e-8),
-#'                         Opt = opt(step_size = bold_driver(),
-#'                                   update = nesterov_non_convex_momentum(),
-#'                                   gradient = nesterov),
-#'                         export=c('In'))
+#' To carry out an embedding, you need to provide quite a bit of information
+#' to parameterise it correctly. Rather then requiring it all be passed as
+#' multiple arguments to the function, logically related pieces are separated
+#' into separate components. These could be as simple as a single function,
+#' or be a list of related functions. Each component has its own factory
+#' function, so you don't need to worry about the underlying structure.
 #'
-#' mnist hajse_m6k <- prob_embed_f(m1k$x, plot_func = mnist_plot(m1k),
-#'                                perplexity = 30,
-#'                                gradient = hajse(alpha = 1, beta = 0.5),
-#'                                Opt = opt(step_size = bold_driver(),
-#'                                          update =
-#'                                            nesterov_non_convex_momentum(),
-#'                                          gradient = nesterov),
-#'                                export = c('In'))
+#' @section Preprocessing:
+#' Entirely optional, but provides some way to preprocess the input data, e.g.
+#' various scaling methods, filtering of zero-variance columns, whitening.
+#' See \code{?make_preprocess} for more details.
 #'
-#' global asymmetric probs
-#' hhasneg_s1k <- prob_embed_f(s1k[, 1:9],
-#'                             plot_func = make_plot_func(s1k, 'Label'),
-#'                             perplexity = 50,
-#'                             gradient = hhasne(alpha = 1, global = TRUE),
-#'                             Opt = opt(step_size = bold_driver(),
-#'                                       update =
-#'                                        nesterov_non_convex_momentum(),
-#'                                       gradient = nesterov),
-#'                             export=c('In'))
+#' @section Input Initialization:
+#' Generates the input matrices used in the embedding. For SNE and related
+#' methods, a probability matrix is needed. There's really only one way of
+#' doing this, which involves specifying a target perplexity for row-wise
+#' probabilities. See \code{?make_init_inp} for more details.
 #'
-#' hasne_m1k <- prob_embed_f(m1k$x, plot_func = mnist_plot(m1k), perplexity=50,
-#'                           gradient = hhasne(alpha = 1, global = TRUE),
-#'                           Opt = opt(step_size = bold_driver(),
-#'                                     update = nesterov_non_convex_momentum(),
-#'                                     gradient = nesterov),
-#'                           export=c('In'), reperplexitize_every = 100)
+#' @section Output Initialization:
+#' Generates the initial embedded coordinates. A random initialization from
+#' a gaussian distribution with a very small standard deviation (e.g. 1e-4) is
+#' very popular. But that makes comparing different results difficult. Using
+#' PCA to generate a two-component scores matrix gives repeatable results with
+#' a computational cost that is entirely reasonable. See \code{?make_init_out}
+#' for more details.
+#'
+#' @section Embedding Method:
+#' I take the view that the essentials of any embedding method (or at least the
+#' ones this package can deal with) is the definition of the gradient. This
+#' in turn depends on the cost function used. For similarity preserving methods
+#' like SNE, the exact form of the gradient is also dependent on the weighting
+#' function that converts distances to probabilities, and how the probability
+#' is defined (e.g. joint versus conditional probabilities). The embedding
+#' method defines the functions needed to do this conversion. For now, the
+#' only available methods are ASNE, SSNE and TSNE. More complicated embedding
+#' methods such as JSE and NeRV can be implemented in this framework, and also
+#' distance-preserving embeddings like MDS and Sammon mapping, which don't
+#' involve probabilities at all. These are all coming soon. If the embedding can
+#' have a stiffness matrix defined for it it should be possible to implement it
+#' as a method in sneer, although it requires a little knowledge of the
+#' internals.
+#'
+#' @section Optimization:
+#' The optimization methods in sneer are composed of four sub-components:
+#' \itemize{
+#'  \item Gradient position: 'Classic' or Nesterov.
+#'  \item Direction: currently only steepest descent is supported.
+#'  \item Step Size: two adaptive step size methods are available:
+#'  the Jacobs method used in the t-SNE paper (also known as the delta-bar-delta
+#'  method), and the bold driver method.
+#'  \item Update: various momentum schedules.
+#' }
+#' This might seem a bit overengineered, but it allows for several optimization
+#' methods: including the Jacobs method used in the t-SNE paper, and Nesterov
+#' Accelerated Gradient, which seems to work very well for embeddings. You could
+#' also implement conjugate gradient within this framework, including backwards
+#' stepping Wolfe line search for step size selection, but you would also need
+#' a good guess for the trace of the Hessian, and NAG works well enough with
+#' the bold driver method for step size selection without all that.
+#' See \code{?make_opt} for more details.
+#'
+#' @section Tricks:
+#' Various embedding methods use different heuristics to speed up convergence.
+#' Only the "early exaggeration" method described in the t-SNE paper is
+#' currently implemented. See \code{?make_tricks} for more details.
+#'
+#' @section Reporter:
+#' Optional functions that will run on a regular schedule during the
+#' optimization. Used for keeping track of the progress of the optimization,
+#' and stopping early if necessary. Also, you can plot the current state of the
+#' embedding. See \code{?make_reporter} for more details.
 #'
 #' @docType package
 #' @name sneer
