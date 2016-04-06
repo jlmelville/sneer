c567fe373271a93e48c31dbb14f5ed0bf79456f7
James Melville <jlmelville@gmail.com>
2016-04-06 08:53:02 -0700

    Add Swiss Roll dataset.

--- R/datasets.R
+++ R/datasets.R
@@ -90,9 +90,7 @@ ball <- function(n = 1000, rad = 1, nlabels = 5) {
 #' \emph{Neurocomputing}, \emph{169}, 246-261.
 helix <- function(n = 1000, rmajor = 2, rminor = 1, nwinds = 8, nlabels = 5) {
   # http://math.stackexchange.com/questions/324527/do-these-equations-create-a-helix-wrapped-into-a-torus
-
   u <- seq(-pi, pi, length.out = n)
-
   w <- rmajor + (rminor * cos(nwinds * u))
   x <- w * cos(u)
   y <- w * sin(u)
@@ -123,4 +121,42 @@ gauss <- function(n = 1000, d = 3, nlabels = 6) {
  dist <- sqrt(rowSums(m * m))
  Label <- as.factor(round((nlabels - 1) * (dist / max(dist))))
  data.frame(m, Label)
+}
+
+#' Swiss Roll Data Set
+#'
+#' Simulation data.
+#'
+#' Creates a series of points randomly sampled from a swiss roll-shaped
+#' manifold: a two-dimensional plane which has been rolled up into a spiral
+#' shape. Or just look at a swiss roll.
+#'
+#' The formula for sampling the x, y and z coordinates used in this dataset is
+#' from that given in the Stochastic Proximity Embedding paper by Agrafiotis
+#' and Xu:
+#' \deqn{x = \phi cos\phi, y = \phi sin \phi, z}
+#' {x = phi * cos(phi), y = phi * sin(phi), z}
+#'
+#' where \eqn{\phi}{phi} and z are random numbers in the intervals [5, 13]
+#' and [0, 10], respectively.
+#'
+#' Labels go from \code{0} to \code{nlabels - 1}. Points are classified into
+#' labels based on their z-coordinate.
+#'
+#' @param n Number of points to create.
+#' @param nlabels Number of labels.
+#' @return Data frame.
+#' @references
+#' Agrafiotis, D. K., & Xu, H. (2002).
+#' A self-organizing principle for learning nonlinear manifolds.
+#' \emph{Proceedings of the National Academy of Sciences}, \emph{99}(25), 15869-15872.
+swiss_roll <- function(n = 1000, nlabels = 6) {
+  phi <- runif(n, min = 5, max = 30)
+  x <- phi * cos(phi)
+  y <- phi * sin(phi)
+  z <- runif(n, max = 10)
+
+  Label <- as.factor(ceiling(((phi - 5) / 25) * nlabels - 1))
+
+  data.frame(x, y, z, Label)
 }
